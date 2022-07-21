1fca0e9beb0d629740a6679ef0a11eaff3aecaec
Igor Dudek <igor.dudek@firma.interia.pl>
2022-07-21 12:49:10 +0200

    feat: create rmq module in lib/common

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -1,3 +1,7 @@
+// Database
 export * from './database/database.module';
 export * from './database/abstract.repository';
 export * from './database/abstract.schema';
+// Rmq
+export * from './rmq/rmq.service';
+export * from './rmq/rmq.module';
